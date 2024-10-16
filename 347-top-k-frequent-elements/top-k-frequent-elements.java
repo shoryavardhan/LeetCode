4237b480e922a47920f9d97bd6d0fbddc81bf1a3
Shorya Khanna <37259858+shoryavardhan@users.noreply.github.com>
2024-10-16 14:16:45 +0530

    Time: 125 ms (5.03%) | Memory: 48.5 MB (59.77%) - LeetSync

---
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -0,0 +1,48 @@
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        /*
+         * need to create buckets of (count,list),
+         * the key will be the count and the value will be
+         * the list of elements occured that many times
+         * 
+         * ex- 2,4,4,5,5
+         * 2 occures 1 times and 4 occures 2 times and 5 also occurs 2 times
+         * then the List would be
+         * 
+         * (0->[]),(1->[2]),(2->[4,5]),(3->[]),(4->[])
+         * 
+         * 0 to 4 because there are 5 elements in the list.
+         * now to get top k we will traverse from the back and return;
+         */
+
+        List<Integer>[] bucket = new List[nums.length + 1];
+        HashMap<Integer, Integer> frequencyMap = new HashMap<>();
+        for (int n : nums) {
+            frequencyMap.put(n, frequencyMap.getOrDefault(n, 0) + 1);
+        }
+        
+        for (int key : frequencyMap.keySet()) {
+            var frequency = frequencyMap.get(key);
+            if (bucket[frequency] == null) {
+                bucket[frequency] = new ArrayList<>();
+            }
+            bucket[frequency].add(key);
+        }
+        for (int i=0;i< nums.length+1;i++) {
+            System.out.println(i+"->"+bucket[i]);
+        }
+
+        int[] res = new int[k];
+        int counter = 0;
+
+      for(int pos = bucket.length-1;pos>=0 && counter<k;pos--){
+        if(bucket[pos]!=null){
+            for(int i:bucket[pos]){
+                res[counter++]=i;
+            }
+        }
+      }
+
+        return res;
+    }
+}
