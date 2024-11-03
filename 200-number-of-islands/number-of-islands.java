70d72547596bffa1a93e23ecc82f2740e0b0b509
Shorya Khanna <37259858+shoryavardhan@users.noreply.github.com>
2024-11-03 09:04:11 +0530

    Time: 3 ms (87.38%) | Memory: 49.3 MB (61.89%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,44 +1,32 @@
 class Solution {
+
     public int numIslands(char[][] grid) {
-        var m = grid.length;
-        var n = grid[0].length;
-        var num = 0;
-        HashSet<Pair<Integer, Integer>> set = new HashSet<>();
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] == '1' && !set.contains(new Pair<>(i, j))) {
-                    set.add(new Pair<>(i, j));
-                    bfs(grid, set, i, j);
-                    num++;
+        int count = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == '1') {
+                    dfs(grid, i, j);
+                    count++;
                 }
             }
         }
-        return num;
+        return count;
     }
 
-    public void bfs(char[][] grid, HashSet<Pair<Integer, Integer>> set, int i, int j) {
-        Queue<Pair<Integer, Integer>> q = new LinkedList<>();
-        var m = grid.length;
-        var n = grid[0].length;
-        q.offer(new Pair<>(i, j));
-        while (!q.isEmpty()) {
-            var pair = q.poll();
-            var x = pair.getKey();
-            var y = pair.getValue();
-            //System.out.println("Visiting: " + x + ", " + y);
-            addNeighbor(q, set, x - 1, y, m, n, grid);
-            addNeighbor(q, set, x, y - 1, m, n, grid);
-            addNeighbor(q, set, x + 1, y, m, n, grid);
-            addNeighbor(q, set, x, y + 1, m, n, grid);
+    public void dfs(char[][] grid, int i, int j) {
+        if (
+            i < 0 ||
+            j < 0 ||
+            i >= grid.length ||
+            j >= grid[0].length ||
+            grid[i][j] == '0'
+        ) {
+            return;
         }
-    }
-
-    private void addNeighbor(Queue<Pair<Integer, Integer>> q, HashSet<Pair<Integer, Integer>> set, int newX, int newY,
-            int m, int n, char[][] grid) {
-        if (newX >= 0 && newY >= 0 && newX < m && newY < n && !set.contains(new Pair<>(newX, newY))
-                && grid[newX][newY] == '1') {
-            q.add(new Pair<>(newX, newY));
-            set.add(new Pair<>(newX, newY));
-        }
+        grid[i][j] = '0';
+        dfs(grid, i + 1, j);
+        dfs(grid, i, j + 1);
+        dfs(grid, i - 1, j);
+        dfs(grid, i, j - 1);
     }
 }
