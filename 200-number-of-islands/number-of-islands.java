bf90f823c2e50868856b43774c389c1546f38433
Shorya Khanna <37259858+shoryavardhan@users.noreply.github.com>
2024-11-03 09:39:33 +0530

    Time: 3 ms (87.38%) | Memory: 49.5 MB (52.39%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,11 +1,17 @@
+
 class Solution {
     public int numIslands(char[][] grid) {
+        return solutionByDFS(grid);
+        // return solutionByBFS(grid);
+    }
+
+    public int solutionByDFS(char[][] grid) {
         var m = grid.length;
         var n = grid[0].length;
         var num = 0;
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (grid[i][j] == '1' ) {
+                if (grid[i][j] == '1') {
                     dfs(grid, i, j, m, n);
                     num++;
                 }
@@ -14,16 +20,61 @@ class Solution {
         return num;
     }
 
+    public int solutionByBFS(char[][] grid) {
+        var m = grid.length;
+        var n = grid[0].length;
+        var num = 0;
+        Queue<Pair<Integer, Integer>> q = new LinkedList<>();
+        HashSet<Pair<Integer, Integer>> set = new HashSet<>();
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == '1' && !set.contains(new Pair<>(i, j))) {
+                    set.add(new Pair<>(i, j));
+                    q.offer(new Pair<>(i, j));
+                    bfs(grid, q, set, i, j);
+                    num++;
+                }
+            }
+        }
+        return num;
+    }
+
     public void dfs(char[][] grid, int newX, int newY, int m, int n) {
-        if (newX < 0 || newY < 0 || newX >= m || newY >= n  || grid[newX][newY] == '0') {
+        if (newX < 0 || newY < 0 || newX >= m || newY >= n || grid[newX][newY] == '0') {
             return;
         }
 
-         grid[newX][newY] = '0';
+        grid[newX][newY] = '0';
         dfs(grid, newX + 1, newY, m, n);
         dfs(grid, newX - 1, newY, m, n);
         dfs(grid, newX, newY + 1, m, n);
         dfs(grid, newX, newY - 1, m, n);
     }
 
+    public void bfs(char[][] grid, Queue<Pair<Integer, Integer>> q,
+            HashSet<Pair<Integer, Integer>> set, int i, int j) {
+
+        var m = grid.length;
+        var n = grid[0].length;
+
+        while (!q.isEmpty()) {
+            var pair = q.poll();
+            var x = pair.getKey();
+            var y = pair.getValue();
+            addNeighbor(q, set, x - 1, y, m, n, grid);
+            addNeighbor(q, set, x, y - 1, m, n, grid);
+            addNeighbor(q, set, x + 1, y, m, n, grid);
+            addNeighbor(q, set, x, y + 1, m, n, grid);
+        }
+    }
+
+    private void addNeighbor(Queue<Pair<Integer, Integer>> q,
+            HashSet<Pair<Integer, Integer>> set, int newX, int newY,
+            int m, int n, char[][] grid) {
+        if (newX >= 0 && newY >= 0 && newX < m && newY < n && !set.contains(new Pair<>(newX, newY))
+                && grid[newX][newY] == '1') {
+            q.add(new Pair<>(newX, newY));
+            set.add(new Pair<>(newX, newY));
+        }
+    }
 }
