8a6f0bf7f772305a539ff73c8fe2bdd3c50de1b3
Shorya Khanna <37259858+shoryavardhan@users.noreply.github.com>
2024-11-15 13:38:53 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (63.43%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
@@ -1,20 +1,27 @@
 class Solution {
     public int minCostClimbingStairs(int[] cost) {
-        int[] memo = new int[cost.length];
-        Arrays.fill(memo,-1);
-        var a = dp(cost, 0,memo);
-        var b = dp(cost, 1,memo);
-        return Math.min(a, b);
-    }
+        for (int i = cost.length - 3; i >= 0; i--) {
+            cost[i] = cost[i] + Math.min(cost[i + 1] , cost[i + 2]);
+        }
+        return Math.min(cost[0], cost[1]);
 
-    public int dp(int[] cost, int i,int[] memo) {
-        if (i >= cost.length) {
-            return 0;
-        }
-        if(memo[i]!=-1){
-            return memo[i];
-        }
-        memo[i]= cost[i] + Math.min(dp(cost, i + 1,memo), dp(cost, i + 2,memo));
-        return memo[i];
     }
+    // public int minCostClimbingStairs(int[] cost) {
+    // int[] memo = new int[cost.length];
+    // Arrays.fill(memo,-1);
+    // var a = dp(cost, 0,memo);
+    // var b = dp(cost, 1,memo);
+    // return Math.min(a, b);
+    // }
+
+    // public int dp(int[] cost, int i,int[] memo) {
+    // if (i >= cost.length) {
+    // return 0;
+    // }
+    // if(memo[i]!=-1){
+    // return memo[i];
+    // }
+    // memo[i]= cost[i] + Math.min(dp(cost, i + 1,memo), dp(cost, i + 2,memo));
+    // return memo[i];
+    // }
 }
