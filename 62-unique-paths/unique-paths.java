7e3209bc47e5a089abfe3a2928978a9691aa2844
Shorya Khanna <37259858+shoryavardhan@users.noreply.github.com>
2024-12-25 01:47:52 +0530

    Time: 0 ms (100.00%) | Memory: 40.4 MB (60.49%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,29 +1,11 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        return tab2(m, n);
-    }
-
-    public int tab2(int m, int n) {
-        var prev = new int[n];
-        for (int i = 0; i < m; i++) {
-            var temp = new int[n];
-            for (int j = 0; j < n; j++) {
-                var up = 0;
-                var left = 0;
-                if (i == 0 && j == 0) {
-                    temp[j] = 1;
-                } else {
-                    if (i > 0) {
-                        up = prev[j];
-                    }
-                    if (j > 0) {
-                        left = temp[j - 1];
-                    }
-                    temp[j] = up + left;
-                }
-            }
-            prev = temp;
+        int N = m + n - 2;
+        int R = m - 1;
+        double res = 1;
+        for (int i = 1; i <= R; i++) {
+            res = res * (N - R + i) / i;
         }
-        return prev[n - 1];
+        return (int) res;
     }
 }
