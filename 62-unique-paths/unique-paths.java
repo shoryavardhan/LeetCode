e728874543f9077dd5afd8fc2b701a2875b26f49
Shorya Khanna <37259858+shoryavardhan@users.noreply.github.com>
2024-12-25 01:38:11 +0530

    Time: 0 ms (100.00%) | Memory: 40.3 MB (60.49%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -2,46 +2,71 @@ class Solution {
     public int uniquePaths(int m, int n) {
         // int[][] memo = new int[m][n];
         // return dp(m, n, m - 1, n - 1, memo);
-        return tab(m, n);
+        // return tab(m, n);
+        return tab2(m, n);
     }
 
-    public int tab(int m, int n) {
-        int[][] memo = new int[m][n];
+    public int tab2(int m, int n) {
+        var prev = new int[n];
         for (int i = 0; i < m; i++) {
+             var temp = new int[n];
             for (int j = 0; j < n; j++) {
                 var up = 0;
                 var left = 0;
                 if (i == 0 && j == 0) {
-                    memo[i][j] = 1;
+                    temp[j] = 1;
                 } else {
                     if (i > 0) {
-                        up = memo[i - 1][j];
+                        up = prev[j];
                     }
                     if (j > 0) {
-                        left = memo[i][j - 1];
+                        left = temp[j - 1];
                     }
-                    memo[i][j] = up + left;
+                    temp[j] = up + left;
                 }
-                
+               
             }
+             prev = temp;
         }
-        return memo[m - 1][n - 1];
+        return prev[n-1];
     }
+    // public int tab(int m, int n) {
+    // int[][] memo = new int[m][n];
+    // for (int i = 0; i < m; i++) {
+    // for (int j = 0; j < n; j++) {
+    // var up = 0;
+    // var left = 0;
+    // if (i == 0 && j == 0) {
+    // memo[i][j] = 1;
+    // } else {
+    // if (i > 0) {
+    // up = memo[i - 1][j];
+    // }
+    // if (j > 0) {
+    // left = memo[i][j - 1];
+    // }
+    // memo[i][j] = up + left;
+    // }
 
-    public int dp(int m, int n, int i, int j, int[][] memo) {
-        if (i == 0 && j == 0) {
-            return 1;
-        }
-        if (i < 0 || j < 0) {
-            return 0;
-        }
-        if (memo[i][j] != 0) {
-            return memo[i][j];
-        }
-        var up = dp(m, n, i - 1, j, memo);
-        var down = dp(m, n, i, j - 1, memo);
-        memo[i][j] = up + down;
+    // }
+    // }
+    // return memo[m - 1][n - 1];
+    // }
 
-        return memo[i][j];
-    }
+    // public int dp(int m, int n, int i, int j, int[][] memo) {
+    // if (i == 0 && j == 0) {
+    // return 1;
+    // }
+    // if (i < 0 || j < 0) {
+    // return 0;
+    // }
+    // if (memo[i][j] != 0) {
+    // return memo[i][j];
+    // }
+    // var up = dp(m, n, i - 1, j, memo);
+    // var down = dp(m, n, i, j - 1, memo);
+    // memo[i][j] = up + down;
+
+    // return memo[i][j];
+    // }
 }
