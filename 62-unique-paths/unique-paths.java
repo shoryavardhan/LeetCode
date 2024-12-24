f6a7a5578233d487190dfffbe91ef822cea0335c
Shorya Khanna <37259858+shoryavardhan@users.noreply.github.com>
2024-12-25 01:01:28 +0530

    Time: 0 ms (100.00%) | Memory: 40.4 MB (60.24%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -4,6 +4,19 @@ class Solution {
         return dp(m, n, m - 1, n - 1, memo);
     }
 
+    public int tab(int m, int n) {
+        int[][] memo = new int[m][n];
+        memo[0][0] = 1;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i != 0 && j != 0) {
+                    memo[i][j] = memo[i - 1][j] + memo[i][j - 1];
+                }
+            }
+        }
+        return memo[m-1][n-1];
+    }
+
     public int dp(int m, int n, int i, int j, int[][] memo) {
         if (i == 0 && j == 0) {
             return 1;
@@ -11,12 +24,12 @@ class Solution {
         if (i < 0 || j < 0) {
             return 0;
         }
-        if(memo[i][j]!=0){
+        if (memo[i][j] != 0) {
             return memo[i][j];
         }
         var up = dp(m, n, i - 1, j, memo);
         var down = dp(m, n, i, j - 1, memo);
-        memo[i][j] = up+down;
+        memo[i][j] = up + down;
 
         return memo[i][j];
     }
