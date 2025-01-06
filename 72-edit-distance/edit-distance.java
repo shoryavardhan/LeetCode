ddd46ea49016b8db51a2bae50959c9bc6b3fb77f
Shorya Khanna <37259858+shoryavardhan@users.noreply.github.com>
2025-01-06 20:45:09 +0530

    Time: 5 ms (64.29%) | Memory: 45.4 MB (16.10%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -1,24 +1,52 @@
 class Solution {
     public int minDistance(String word1, String word2) {
-        int[][] memo = new int[word1.length()+1][word2.length()+1];
-        for(int i = 0; i<=word1.length();i++){
-            Arrays.fill(memo[i],-1);
+        return bottomUp(word1, word2);
+    }
+
+    public int bottomUp(String word1, String word2) {
+        int[][] memo = new int[word1.length() + 1][word2.length() + 1];
+        for (int i = 0; i <= word1.length(); i++) {
+            memo[i][0] = i;
+        }
+        for (int j = 0; j <= word2.length(); j++) {
+            memo[0][j] = j;
+        }
+        for (int i = 1; i <= word1.length(); i++) {
+            for (int j = 1; j <= word2.length(); j++) {
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    memo[i][j] = 0 + memo[i - 1][j - 1];
+                } else {
+                    var replaced = 1 + memo[i - 1][j - 1];
+                    var deleted = 1 + memo[i - 1][j];
+                    var added = 1 + memo[i][j - 1];
+                    memo[i][j] = Math.min(replaced, Math.min(deleted, added));
+                }
+            }
+        }
+
+        return memo[word1.length()][word2.length()];
+    }
+
+    public int topDown(String word1, String word2) {
+        int[][] memo = new int[word1.length() + 1][word2.length() + 1];
+        for (int i = 0; i <= word1.length(); i++) {
+            Arrays.fill(memo[i], -1);
         }
         return dp(word1, word2, word1.length(), word2.length(), memo);
     }
 
     public int dp(String word1, String word2, int i, int j, int[][] memo) {
-        if (i == 0 ){
+        if (i == 0) {
             return j;
         }
-        if(j == 0) {
+        if (j == 0) {
             return i;
         }
-        if(memo[i][j]!=-1){
+        if (memo[i][j] != -1) {
             return memo[i][j];
         }
         if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
-            memo[i][j] =  0 + dp(word1, word2, i - 1, j - 1, memo);
+            memo[i][j] = 0 + dp(word1, word2, i - 1, j - 1, memo);
         } else {
             var replaced = 1 + dp(word1, word2, i - 1, j - 1, memo);
             var deleted = 1 + dp(word1, word2, i - 1, j, memo);
