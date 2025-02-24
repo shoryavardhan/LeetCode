e0fda603dbb9229943baebb4269b2ab367d1d106
Shorya Khanna <37259858+shoryavardhan@users.noreply.github.com>
2025-02-24 15:16:29 +0530

    Time: 37 ms (20.07%) | Memory: 45.6 MB (7.06%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -1,31 +1,28 @@
 class Solution {
     public int characterReplacement(String s, int k) {
-        int res = 0;
-        int l = 0, r = 0;
-        var countMap = new HashMap<Character, Integer>();
-        int maxCount = 0; // Tracks the count of the most frequent character in the current window
+        var n = s.length();
+        var map = new HashMap<Character, Integer>();
 
-        while (r < s.length()) {
-            Character c = s.charAt(r);
-            countMap.put(c, countMap.getOrDefault(c, 0) + 1);
-            maxCount = Math.max(maxCount, countMap.get(c));
+        var l = 0;
+        var r = 0;
+        var maxc = 0;
+        var res = 0;
 
-            // Calculate the window length
-            int winLen = r - l + 1;
+        while (r < n) {
+            var curr = s.charAt(r);
+            map.put(curr, map.getOrDefault(curr, 0) + 1);
+            maxc = Math.max(maxc, map.get(curr));
 
-            // If the number of characters we need to replace is more than k, shrink the window from the left
-            if (winLen - maxCount > k) {
-                Character leftChar = s.charAt(l);
-                countMap.put(leftChar, countMap.get(leftChar) - 1);
+            var winLen = r - l + 1;
+
+            if (winLen - maxc > k) {
+                map.put(s.charAt(l), map.get(s.charAt(l))-1);
                 l++;
             }
-
-            // Update result if the current window length is valid
             res = Math.max(res, r - l + 1);
-
             r++;
         }
-
         return res;
+
     }
 }
