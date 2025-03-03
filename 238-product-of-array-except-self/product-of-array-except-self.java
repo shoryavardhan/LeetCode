989824dae6469503ff34f10dd2edb9a82e2165fb
Shorya Khanna <37259858+shoryavardhan@users.noreply.github.com>
2025-03-04 00:27:38 +0530

    Time: 2 ms (87.91%) | Memory: 55.4 MB (62.25%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -1,36 +1,20 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int n = nums.length;
-        int[] result = new int[n];
-        for (int i = 0; i < n; i++) {
-            result[i] = 1;
+        // 1 2 3 4
+        // 1 1 2 6
+        // 24 12 4 1
+        // 24 12 8 6
+        var n = nums.length;
+        var p = new int[n];
+        p[0] = 1;
+        for (int i = 1; i < n; i++) {
+            p[i] = p[i - 1] * nums[i - 1];
         }
-        int p = 1;
-        for (int i = 0; i < n; i++) {
-            result[i] = result[i] * p;
-            p = p * nums[i];
-        }
-        int s = 1;
+        var s = 1;
         for (int i = n - 1; i >= 0; i--) {
-            result[i] = result[i] * s;
+            p[i] = p[i] * s;
             s = s * nums[i];
         }
-        return result;
+        return p;
     }
 }
-
-/*
- * array -> 1. 2. 3. 4.
- * prefix -> 1 2 6 24
- * suffix-> 24 24 12 4
- * 
- * 
- * but if we move prefix a bit ahead and pull suffix it will give us what we
- * need like
- * 
- * array -> 1 2 3 4
- * prefix -> 1 1 2 6 24
- * suffix-> 24 24 12 4 1
- * result -> 24 12 8 6
- * 
- */
